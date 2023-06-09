d5189f8497df7ff8f4e30a421c9a1c63d741bb63
guohan <guohan@shiguangjk.com>
2023-06-09 18:44:17 +0800

    feat:init

--- core/dependency/ezcurl/EzCurl.php
+++ core/dependency/ezcurl/EzCurl.php
@@ -261,7 +261,7 @@ class EzCurl
     }
 
     public function setFromEdge() {
-        return $this->setUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1823.37");
+        return $this->setUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1823.43");
     }
 
     public function prepare()
