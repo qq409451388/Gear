a29a5ef150aff368c4058ec66d6ac905d4043efd
guohan <guohan@shiguangjk.com>
2023-06-19 19:47:20 +0800

    feat: EzRouter支持动态参数构建

--- core/annoation/lombok/EzLogger.php
+++ core/annoation/lombok/EzLogger.php
@@ -1,6 +1,9 @@
 <?php
 class EzLogger extends Anno
 {
+    /**
+     * 指定注解可以放置的位置（默认: 所有）@see AnnoElementType
+     */
     public static function constTarget()
     {
         return AnnoElementType::TYPE_METHOD;
