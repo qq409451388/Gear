2ef6d26d573ad2d3f40248d1d65132410b8f83ad
guohan <guohan@shiguangjk.com>
2023-08-26 15:26:35 +0800

    feat: Database Data check

--- modules/db/BaseDB.php
+++ modules/db/BaseDB.php
@@ -11,7 +11,7 @@ abstract class BaseDB extends BaseDBSimple implements IDbSe
             if (is_null($v)) {
                 $vals .= "null";
             } else {
-                $vals .= is_integer($v) ? $v : '"'.$v.'"';
+                $vals .= is_integer($v) ? $v : "'".$v."'";
             }
             $vals .= ",";
         }
@@ -37,7 +37,7 @@ abstract class BaseDB extends BaseDBSimple implements IDbSe
         foreach($infos as $info){
             $vals .= "(";
             foreach($info as $k => $v){
-                $vals .= is_integer($v) ? $v : '"'.$v.'"';
+                $vals .= is_integer($v) ? $v : "'".$v."'";
                 $vals .= ",";
             }
             $vals = trim($vals, ",");
@@ -93,7 +93,29 @@ abstract class BaseDB extends BaseDBSimple implements IDbSe
         if(null == $dbInfo){
             $dbInfo = $this->query("desc $db", [], SqlOptions::new()->setUseCache(true));
         }
+        $primaryKey = $this->analyzePrimaryKey($dbInfo);
+        $this->preCheckPrimaryKey4Write($primaryKey, $info);
         $columns = array_column($dbInfo, "Field");
+        $this->preCheckUnknowKeys4Write($columns, $info);
+        $this->prepare($dbInfo, $info);
+        $this->checkFields($dbInfo, $info);
+    }
+
+    private function analyzePrimaryKey($dbInfo){
+        $primaryKey = array_filter($dbInfo, function($dbInfoItem){
+            return $dbInfoItem['Key'] == 'PRI';
+        });
+        $primaryKey = current($primaryKey);
+        return empty($primaryKey) ? null : $primaryKey["Field"];
+    }
+
+    private function preCheckPrimaryKey4Write($primaryKey, $info) {
+        if (!is_null($primaryKey) && array_key_exists($primaryKey, $info)) {
+            DBC::assertNonNull($info[$primaryKey], "[DB Exception] Primary Key Can Not Be Null If it is specified");
+        }
+    }
+
+    private function preCheckUnknowKeys4Write($columns, $info) {
         $diffColumns = array_diff(array_keys($info), $columns);
         DBC::assertEmpty($diffColumns, "[DB Exception] Unknow Columns ".implode(",", $diffColumns));
         $mustExistsColumns = array_column(array_filter($columns, function($val){
@@ -101,10 +123,9 @@ abstract class BaseDB extends BaseDBSimple implements IDbSe
         }),"Field");
         $diffColumns = array_diff($mustExistsColumns, $columns);
         DBC::assertEmpty($diffColumns, "[DB Exception] Must Set Columns ".implode(",", $diffColumns));
+    }
 
-        $dbInfoHash = array_column($dbInfo, null, "Field");
-        $this->checkFields($info, $dbInfoHash);
-
+    private function prepare($dbInfo, $info) {
         $timeStampKeys = array_column(array_filter($dbInfo, function($dbInfoItem){
             return $dbInfoItem['Type'] == 'timestamp' ? $dbInfoItem : null;
         }), "Field");
@@ -124,29 +145,42 @@ abstract class BaseDB extends BaseDBSimple implements IDbSe
                 $value = $value->datetimeString();
             } else if ($value instanceof SqlJsonDataItem) {
                 $value = $value->getJson();
+                $value = trim(json_encode($value), "\"");
+            } else if (in_array($column, $jsonKeys)) {
+                $value = EzString::encodeJson($value);
                 $value = str_replace("\"", "\\\"", $value);
             } else {
-                if (in_array($column, $jsonKeys)) {
-                    $value = EzString::encodeJson($value);
-                    $value = str_replace("\"", "\\\"", $value);
-                } else {
-                    $value = htmlentities($value);
-                }
+                //$value = htmlentities($value);
             }
         }
     }
 
     private function preCheck4Write4Multi(string $db, array &$infos){
         $dbInfo = $this->query("desc $db", [], SqlOptions::new()->setUseCache(true));
-        //todo 检查每一个info的key都是一样的
+        $keys = [];
+        foreach ($infos as $info) {
+            $keys = array_merge($keys, array_keys($info));
+        }
+        $keys = array_unique($keys);
+
         foreach($infos as &$info){
+            $this->rebuildSaveData($keys, $info);
             $this->preCheck4Write($db, $info, $dbInfo);
         }
     }
 
-    private function checkFields($itemData, $fieldInfos) {
+    private function rebuildSaveData($keys, &$itemData) {
+        $newItemData = [];
+        foreach ($keys as $key) {
+            $newItemData[$key] = $itemData[$key]??null;
+        }
+        $itemData = $newItemData;
+    }
+
+    private function checkFields($dbInfo, $itemData) {
+        $dbInfoHash = array_column($dbInfo, null, "Field");
         foreach ($itemData as $k => $v) {
-            $fieldInfo = $fieldInfos[$k];
+            $fieldInfo = $dbInfoHash[$k];
             if ("NO" === $fieldInfo['Null']) {
                 DBC::assertNonNull($v, "[DB Exception] Column $k Can Not Be Null");
             }
@@ -154,56 +188,73 @@ abstract class BaseDB extends BaseDBSimple implements IDbSe
                 continue;
             }
 
-            preg_match("/(?<type>[\/a-zA-Z0-9]+)\(?(?<length>\d+)?\)?(\s+)?(?<unsigned>unsigned)?/", strtolower($fieldInfo['Type']), $matches);
-            $type = $matches['type'];
-            $length = $matches['length'] ?? 0;
-            $unsigned = $matches['unsigned']??"" == 'unsigned';
-            switch ($type) {
-                case "char":
-                case "varchar":
-                    DBC::assertLessThan($length, strlen($v), "[DB Exception] Column $k Length Must Less Than $length but sent ".EzObjectUtils::toString($v));
-                    break;
-                case "text":
-                    $valueActualLen = $v instanceof SqlJsonDataItem ? $v->getJsonLength() : strlen($v);
-                    DBC::assertLessThan(65535, $valueActualLen, "[DB Exception] Column $k Length Must Less Than 65535 but sent ".EzObjectUtils::toString($v));
-                    break;
-                case "tinyint":
-                    DBC::assertNumeric($v, "[DB Exception] Column $k Must Be Numeric");
-                    if ($unsigned) {
-                        DBC::assertInRange("[0, 256)", $v, "[DB Exception] The value of Column $k Must in range 0~255 but sent ".EzObjectUtils::toString($v));
-                    } else {
-                        DBC::assertInRange("[-128, 128)", $v, "[DB Exception] The value of Column $k Must in range -128~127 but sent ".EzObjectUtils::toString($v));
-                    }
-                    break;
-                case "int":
-                case "smallint":
-                case "mediumint":
-                    DBC::assertNumeric($v, "[DB Exception] Column $k Must Be Numeric");
-                    if ($unsigned) {
-                        DBC::assertInRange("[0,18446744073709551615)", $v, "[DB Exception] The value of Column $k Must in range 0~255 but sent ".EzObjectUtils::toString($v));
-                    } else {
-                        DBC::assertInRange("[-9223372036854775808,9223372036854775807)", $v, "[DB Exception] The value of Column $k Must in range -128~127 but sent ".EzObjectUtils::toString($v));
-                    }
-                    break;
-                case "bigint":
-                    DBC::assertNumeric($v, "[DB Exception] Column $k Must Be Numeric");
-                    if ($unsigned) {
-                        DBC::assertInRange("[0,18446744073709551615)", $v, "[DB Exception] The value of Column $k Must in range 0~18446744073709551615 but sent ".EzObjectUtils::toString($v));
-                    } else {
-                        DBC::assertInRange("[-9223372036854775808,9223372036854775808)", $v, "[DB Exception] The value of Column $k Must in range -9223372036854775808~9223372036854775808 but sent ".EzObjectUtils::toString($v));
-                    }
-                    break;
-                case "datetime":
-                    DBC::assertTrue(EzDateUtils::isValid($v), "[DB Exception] Column $k Must Be A Valid Datetime");
-                    break;
-                case "date":
-                    DBC::assertTrue(EzDateUtils::isValid($v), "[DB Exception] Column $k Must Be A Valid Date");
-                    break;
-                case "decimal":
-                    break;
-                default:
-                    Logger::warn("[DB Exception] Column $k Type {$fieldInfo['Type']} Not Check, sendType $type");
-            }
+            $this->checkValue($fieldInfo, $k, $v);
+        }
+    }
+
+    private function checkValue($fieldInfo, $k, $v) {
+        preg_match("/(?<type>[\/a-zA-Z0-9]+)\(?(?<length>\d+)?\)?(\s+)?(?<unsigned>unsigned)?/", strtolower($fieldInfo['Type']), $matches);
+        $type = $matches['type'];
+        $length = $matches['length'] ?? 0;
+        $unsigned = $matches['unsigned']??"" == 'unsigned';
+        switch ($type) {
+            case "char":
+            case "varchar":
+                DBC::assertLessThan($length, strlen($v), "[DB Exception] Column $k Length Must Less Than $length but sent ".EzObjectUtils::toString($v));
+                break;
+            case "text":
+                $valueActualLen = $v instanceof SqlJsonDataItem ? $v->getJsonLength() : strlen($v);
+                DBC::assertLessThan(65535, $valueActualLen, "[DB Exception] Column $k Length Must Less Than 65535 but sent ".EzObjectUtils::toString($v));
+                break;
+            case "tinyint":
+                DBC::assertNumeric($v, "[DB Exception] Column $k Must Be Numeric");
+                if ($unsigned) {
+                    DBC::assertInRange("[0, 256)", $v, "[DB Exception] The value of Column $k Must in range 0~255 but sent ".EzObjectUtils::toString($v));
+                } else {
+                    DBC::assertInRange("[-128, 128)", $v, "[DB Exception] The value of Column $k Must in range -128~127 but sent ".EzObjectUtils::toString($v));
+                }
+                break;
+            case "int":
+                DBC::assertNumeric($v, "[DB Exception] Column $k Must Be Numeric");
+                if ($unsigned) {
+                    DBC::assertInRange("[0, 4294967296)", $v, "[DB Exception] The value of Column $k Must in range 0~4294967295 but sent ".EzObjectUtils::toString($v));
+                } else {
+                    DBC::assertInRange("[-2147483648, 2147483648)", $v, "[DB Exception] The value of Column $k Must in range -2147483648~2147483647 but sent ".EzObjectUtils::toString($v));
+                }
+                break;
+            case "smallint":
+                DBC::assertNumeric($v, "[DB Exception] Column $k Must Be Numeric");
+                if ($unsigned) {
+                    DBC::assertInRange("[0, 65536)", $v, "[DB Exception] The value of Column $k Must in range 0~65535 but sent ".EzObjectUtils::toString($v));
+                } else {
+                    DBC::assertInRange("[-32768, 32768)", $v, "[DB Exception] The value of Column $k Must in range -32768~32767 but sent ".EzObjectUtils::toString($v));
+                }
+                break;
+            case "mediumint":
+                DBC::assertNumeric($v, "[DB Exception] Column $k Must Be Numeric");
+                if ($unsigned) {
+                    DBC::assertInRange("[0,16777216)", $v, "[DB Exception] The value of Column $k Must in range 0~16777215 but sent ".EzObjectUtils::toString($v));
+                } else {
+                    DBC::assertInRange("[-8388608,8388608)", $v, "[DB Exception] The value of Column $k Must in range -8388608~8388607 but sent ".EzObjectUtils::toString($v));
+                }
+                break;
+            case "bigint":
+                DBC::assertNumeric($v, "[DB Exception] Column $k Must Be Numeric");
+                if ($unsigned) {
+                    DBC::assertInRange("[0,18446744073709551615)", $v, "[DB Exception] The value of Column $k Must in range 0~18446744073709551615 but sent ".EzObjectUtils::toString($v));
+                } else {
+                    DBC::assertInRange("[-9223372036854775808,9223372036854775808)", $v, "[DB Exception] The value of Column $k Must in range -9223372036854775808~9223372036854775808 but sent ".EzObjectUtils::toString($v));
+                }
+                break;
+            case "datetime":
+            case "timestamp":
+            case "date":
+                DBC::assertTrue(EzDateUtils::isValid($v), "[DB Exception] Column $k Must Be A Valid Datetime");
+                break;
+            case "decimal":
+                break;
+            default:
+                Logger::warn("[DB Exception] Column $k Type {$fieldInfo['Type']} Not Check, sendType $type");
         }
     }
 }
